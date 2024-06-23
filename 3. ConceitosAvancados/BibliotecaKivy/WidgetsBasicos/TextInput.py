400ad371d6d87db2a9f172dd8cd11ee0b2730294
Danillo <danillordm@gmail.com>
2024-06-23 14:33:30 -0300

    Fix

--- 3. ConceitosAvancados/BibliotecaKivy/WidgetsBasicos/TextInput.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.textinput import TextInput
-
-class MeuApp(App):
-    def build(self):
-        return TextInput(text='Digite aqui')
-
-if __name__ == '__main__':
-    MeuApp().run()
